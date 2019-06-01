40688f454b6bcc13a012853219bb041d2cad12e4
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-06-01 22:58:34 +0530

    added mui components

--- packages/demo-app/src/pages/404.js
+++ packages/demo-app/src/pages/404.js
@@ -2,13 +2,32 @@ import React from 'react';
 
 import { SEO } from 'core-theme';
 import { Layout } from 'mui-theme';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-const NotFoundPage = () => (
-	<Layout>
-		<SEO title="404: Not found" />
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-	</Layout>
-);
+const useStyles = makeStyles({
+	heading: {
+		fontWeight: 'bold'
+	}
+});
+const NotFoundPage = () => {
+	const classes = useStyles();
+	return (
+		<Layout>
+			<SEO title="404: Not found" />
+			<Typography
+				variant="h4"
+				gutterBottom
+				color="primary"
+				className={classes.heading}
+			>
+				NOT FOUND
+			</Typography>
+			<Typography variant="body1" component="p" gutterBottom>
+				You just hit a route that doesn&#39;t exist... the sadness.
+			</Typography>
+		</Layout>
+	);
+};
 
 export default NotFoundPage;
