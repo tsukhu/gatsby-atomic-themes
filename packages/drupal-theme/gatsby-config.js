94c266a26858a718167a9db286b25add8a9dcd62
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-06-15 15:35:25 +0530

    drupal configuration

---
+++ packages/drupal-theme/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = (themeOptions) => {
+	return {
+		plugins: [
+			{
+				resolve: `gatsby-plugin-compile-es6-packages`,
+				options: {
+					modules: ['@gatsby-themes/drupal']
+				}
+			},
+			{
+				resolve: `gatsby-source-drupal`,
+				options: {
+					...themeOptions.drupal
+				}
+			}
+		]
+	};
+};
