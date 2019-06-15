280233d1dc8713bc5ac6ca91b7e7bd1db50f534a
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-06-15 17:07:08 +0530

    added emotion themes

--- packages/drupal-app/src/pages/index.js
+++ packages/drupal-app/src/pages/index.js
@@ -15,3 +15,97 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+	query {
+		topRecipe: allRecipes(sort: { fields: [createdAt] }, limit: 1) {
+			edges {
+				node {
+					title
+					fields {
+						slug
+					}
+					relationships {
+						image {
+							relationships {
+								imageFile {
+									localFile {
+										childImageSharp {
+											fluid(maxWidth: 740, maxHeight: 555) {
+												...GatsbyImageSharpFluid
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		nextTwoPromotedRecipes: allRecipes(
+			sort: { fields: [createdAt] }
+			limit: 2
+			skip: 1
+		) {
+			edges {
+				node {
+					title
+					fields {
+						slug
+					}
+					relationships {
+						category {
+							name
+						}
+						image {
+							relationships {
+								imageFile {
+									localFile {
+										childImageSharp {
+											fluid(maxWidth: 240, maxHeight: 240) {
+												...GatsbyImageSharpFluid
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		nextFourPromotedRecipes: allRecipes(
+			sort: { fields: [createdAt] }
+			limit: 4
+			skip: 3
+		) {
+			edges {
+				node {
+					title
+					fields {
+						slug
+					}
+					relationships {
+						category {
+							name
+						}
+						image {
+							relationships {
+								imageFile {
+									localFile {
+										childImageSharp {
+											fluid(maxWidth: 475, maxHeight: 475) {
+												...GatsbyImageSharpFluid
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`;
