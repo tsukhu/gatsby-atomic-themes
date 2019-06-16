db9100a76ede4007cbb5d1e09123049441433401
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-06-16 11:12:27 +0530

    added global styles

--- packages/drupal-app/src/@gatsby-themes/core/components/layout.js
+++ packages/drupal-app/src/@gatsby-themes/core/components/layout.js
@@ -8,64 +8,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
-import { ThemeProvider, withTheme } from 'emotion-theming'
+import { ThemeProvider, withTheme } from 'emotion-theming';
+import { Global, css } from '@emotion/core';
 import { Link } from 'gatsby';
 import tokens from '../../../tokens/theme';
 
-const Layout = ({ children}) => {
+const Layout = ({ children }) => {
 	return (
-	<StaticQuery
-		query={graphql`
-			query SiteTitleQueryDrupalApp {
-				site {
-					siteMetadata {
-						title
+		<>
+			<Global
+				styles={css`
+					html,
+					body {
+						font-family: 'Montserrat';
 					}
-				}
-			}
-		`}
-		render={(data) =>  (
-			<ThemeProvider theme={tokens}>
-				<header>
-					<div
-						style={{
-							margin: `0 auto`,
-							maxWidth: 960,
-							padding: `1.45rem 1.0875rem`
-						}}
-					>
-						<h1 style={{ margin: 0}}>
-							<Link
-								to="/"
+				`}
+			/>
+			<StaticQuery
+				query={graphql`
+					query SiteTitleQueryDrupalApp {
+						site {
+							siteMetadata {
+								title
+							}
+						}
+					}
+				`}
+				render={(data) => (
+					<ThemeProvider theme={tokens}>
+						<header>
+							<div
 								style={{
-									color: tokens.color.primary,
-									textDecoration: `none`
+									margin: `0 auto`,
+									maxWidth: 960,
+									padding: `1.45rem 1.0875rem`
 								}}
 							>
-								{data.site.siteMetadata.title}
-							</Link>
-						</h1>
-					</div>
-				</header>
-				<div
-					style={{
-						margin: `0 auto`,
-						maxWidth: 960,
-						padding: `0px 1.0875rem 1.45rem`,
-						paddingTop: 0
-					}}
-				>
-					<main>{children}</main>
-					<footer>
-						© {new Date().getFullYear()}, Built with
-						{` `}
-						<a href="https://www.gatsbyjs.org">Gatsby</a>
-					</footer>
-				</div>
-			</ThemeProvider>
-		)}
-	/>
-)};
+								<h1 style={{ margin: 0 }}>
+									<Link
+										to="/"
+										style={{
+											color: tokens.color.primary,
+											textDecoration: `none`
+										}}
+									>
+										{data.site.siteMetadata.title}
+									</Link>
+								</h1>
+							</div>
+						</header>
+						<div
+							style={{
+								margin: `0 auto`,
+								maxWidth: 960,
+								padding: `0px 1.0875rem 1.45rem`,
+								paddingTop: 0
+							}}
+						>
+							<main>{children}</main>
+							<footer>
+								© {new Date().getFullYear()}, Built with
+								{` `}
+								<a href="https://www.gatsbyjs.org">Gatsby</a>
+							</footer>
+						</div>
+					</ThemeProvider>
+				)}
+			/>
+		</>
+	);
+};
 
 Layout.propTypes = {
 	children: PropTypes.node.isRequired
