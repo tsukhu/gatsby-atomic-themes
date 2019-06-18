d09a1b52a71142d8fdbc871574c73fb5e704ec42
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-06-18 18:22:33 +0530

    streamlined Receipes

--- packages/drupal-app/gatsby-node.js
+++ packages/drupal-app/gatsby-node.js
@@ -4,7 +4,7 @@ const path = require(`path`);
 exports.onCreateNode = ({ node, getNode, actions }) => {
 	const { createNodeField } = actions;
 	if (node.internal.type === `recipes`) {
-		const slug = `/recipes/${node.internalId}/`;
+		const slug = `/${node.internalId}/`;
 		createNodeField({
 			node,
 			name: `slug`,
--- packages/drupal-app/src/pages/index.js
+++ packages/drupal-app/src/pages/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby';
 import { Layout, SEO } from '@gatsby-themes/core';
 
-const IndexPage = ({data}) => {
-	const topRecipe = data.topRecipe.edges[0].node;
-	const nextTwoPromotedRecipes = data.nextTwoPromotedRecipes.edges.map(
-		(edge) => edge.node
-	);
-	const nextFourPromotedRecipes = data.nextFourPromotedRecipes.edges.map(
-		(edge) => edge.node
-	);
+const IndexPage = ({ data }) => {
 	return (
 		<Layout>
 			<SEO title="Home" />
-			<p>Hi from Drupal</p>
-	{/* 		<p>{topRecipe}</p>
-			<p>{nextTwoPromotedRecipes}</p>
-			<p>{nextFourPromotedRecipes}</p> */}
+			<h1>Recipes</h1>
+			<ul>
+				{data.allRecipes.edges.map(({ node }) => (
+					<li key={node.fields.slug}>
+						<Link to={node.fields.slug}>{node.title}</Link>
+					</li>
+				))}
+			</ul>
 		</Layout>
 	);
 };
@@ -25,7 +22,7 @@ export default IndexPage;
 
 export const pageQuery = graphql`
 	query {
-		topRecipe: allRecipes(sort: { fields: [createdAt] }, limit: 1) {
+		allRecipes(limit: 1000) {
 			edges {
 				node {
 					title
@@ -38,71 +35,7 @@ export const pageQuery = graphql`
 								imageFile {
 									localFile {
 										childImageSharp {
-											fluid(maxWidth: 740, maxHeight: 555) {
-												...GatsbyImageSharpFluid
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		nextTwoPromotedRecipes: allRecipes(
-			sort: { fields: [createdAt] }
-			limit: 2
-			skip: 1
-		) {
-			edges {
-				node {
-					title
-					fields {
-						slug
-					}
-					relationships {
-						category {
-							name
-						}
-						image {
-							relationships {
-								imageFile {
-									localFile {
-										childImageSharp {
-											fluid(maxWidth: 240, maxHeight: 240) {
-												...GatsbyImageSharpFluid
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		nextFourPromotedRecipes: allRecipes(
-			sort: { fields: [createdAt] }
-			limit: 4
-			skip: 3
-		) {
-			edges {
-				node {
-					title
-					fields {
-						slug
-					}
-					relationships {
-						category {
-							name
-						}
-						image {
-							relationships {
-								imageFile {
-									localFile {
-										childImageSharp {
-											fluid(maxWidth: 475, maxHeight: 475) {
+											fluid(maxWidth: 470, maxHeight: 353) {
 												...GatsbyImageSharpFluid
 											}
 										}
