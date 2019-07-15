d9f2a5123e014873803b5d9f400d16cc2d637a94
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-07-15 11:20:54 +0530

    removed extra code from emotion theme

--- packages/emotion-theme/src/stories/index.stories.js
+++ packages/emotion-theme/src/stories/index.stories.js
@@ -1,37 +1,9 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import { css } from '@emotion/core';
-import { cardsList } from '../components/cards';
-import ImageCard from '../components/image-card';
-import RecipeTemplate from '../components/recipe-template';
-import recipeContent from './recipeContent';
-import cardListContent from './cardListContent';
+import About from '../components/about'
 
 storiesOf('Emotion UI Theme', module)
-	.add('Image Card', () => (
-		<div css={cardsList}>
-			<ImageCard
-				slug="my slug"
-				title="Sample Card"
-				imageSrc="http://placeimg.com/640/360/any"
-				cardClickHandler={action('clicked')}
-			/>
-		</div>
-	))
-	.add('Image Card Grid', () => (
-		<div css={cardsList}>
-			{cardListContent.map(({ title, slug, imageSrc }) => (
-				<ImageCard
-					key={title}
-					slug={slug}
-					title={title}
-					imageSrc={imageSrc}
-					cardClickHandler={action('clicked')}
-				/>
-			))}
-		</div>
-	))
-	.add('Receipe Card', () => <RecipeTemplate data={recipeContent} />);
+	.add('About', () => (
+		<About />
+	));
