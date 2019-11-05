611c018bf0d926dfbb6466327a852cce20ad8b50
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-11-05 17:34:19 +0530

    added carousel buttons

--- packages/disney-clone-app/src/components/carousel-wrapper.js
+++ packages/disney-clone-app/src/components/carousel-wrapper.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Carousel from 'nuka-carousel';
 
-const CarouselWrapper = ({ name,data, ...props }) => {
+const CarouselWrapper = ({ name, data, ...props }) => {
 	const [nukaRef, SetNukaRef] = useState(null);
 
 	const onImageLoad = () => {
@@ -26,6 +26,41 @@ const CarouselWrapper = ({ name,data, ...props }) => {
 	return (
 		<Carousel
 			{...props}
+			renderBottomCenterControls={null}
+			renderCenterLeftControls={({ previousSlide, currentSlide }) => (
+				<div class="text-white">
+					{currentSlide !== 0 && (
+						<button onClick={previousSlide} class="opacity-50 hover:opacity-100">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								width="50"
+								height="50"
+								class="fill-current"
+							>
+								<path d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z" />
+							</svg>
+						</button>
+					)}
+				</div>
+			)}
+			renderCenterRightControls={({ nextSlide, slideCount, currentSlide }) => (
+				<div class="text-white">
+					{currentSlide !== slideCount - 1 && (
+						<button onClick={nextSlide} class="opacity-50 hover:opacity-100">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								width="50"
+								height="50"
+								class="fill-current"
+							>
+								<path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+							</svg>
+						</button>
+					)}
+				</div>
+			)}
 			ref={(ref) => {
 				SetNukaRef(ref);
 			}}
--- packages/disney-clone-app/src/pages/index.js
+++ packages/disney-clone-app/src/pages/index.js
@@ -66,40 +66,45 @@ const IndexPage = () => {
 				ref={(ref) => {
 					SetNukaRef(ref);
 				}}
-/* 				renderCenterLeftControls={({ previousSlide }) => (
+				renderBottomCenterControls={null}
+				renderCenterLeftControls={({ previousSlide, currentSlide }) => (
 					<div class="text-white">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							width="24"
-							height="24"
-							class="fill-current"
-						>
-							<path
-								class="heroicon-ui"
-								d="M5.41 11H21a1 1 0 0 1 0 2H5.41l5.3 5.3a1 1 0 0 1-1.42 1.4l-7-7a1 1 0 0 1 0-1.4l7-7a1 1 0 0 1 1.42 1.4L5.4 11z"
-							/>
-						</svg>
+						{currentSlide !== 0 && (
+							<button onClick={previousSlide} class="opacity-50 hover:opacity-100">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									width="50"
+									height="50"
+									class="fill-current"
+								>
+									<path d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z"/>
+								</svg>
+							</button>
+						)}
 					</div>
 				)}
-				renderCenterRightControls={({ nextSlide}) => (
+				renderCenterRightControls={({
+					nextSlide,
+					slideCount,
+					currentSlide
+				}) => (
 					<div class="text-white">
-						<button onClick={nextSlide}>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								width="24"
-								height="24"
-								class="fill-current"
-							>
-								<path
-									class="heroicon-ui"
-									d="M18.59 13H3a1 1 0 0 1 0-2h15.59l-5.3-5.3a1 1 0 1 1 1.42-1.4l7 7a1 1 0 0 1 0 1.4l-7 7a1 1 0 0 1-1.42-1.4l5.3-5.3z"
-								/>
-							</svg>
-						</button>
+						{currentSlide !== slideCount - 1 && (
+							<button onClick={nextSlide} class="opacity-50 hover:opacity-100">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									width="50"
+									height="50"
+									class="fill-current"
+								>
+									<path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/>
+								</svg>
+							</button>
+						)}
 					</div>
-				)} */
+				)}
 			>
 				{slides}
 			</Carousel>
